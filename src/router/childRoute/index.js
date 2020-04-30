e43856e82b94b87b77eafb6833a99237890827cc
mods <dsmo@chinaums.com>
2020-04-30 17:30:27 +0800

    去掉特殊异步路由

--- src/router/childRoute/index.js
+++ src/router/childRoute/index.js
@@ -1,4 +1,44 @@
-import general from './general'
-import special from './special'
+/**
+ * 异步路由
+ * 规则：路由地址（短横线）由组件名称（大驼峰）转换得到
+ */
+import components from './components'
 
-export default [...general, ...special]
+const routes = []
+
+function loadView (view) {
+  return () => import(
+    /* webpackChunkName: '[index]' */
+    `@/views/${view}`
+  )
+}
+
+// 驼峰转短横线 dictList => dict-list
+function getKebabCase (str) {
+  if (!str) return ''
+  return str.replace(/[A-Z]/g, function (s, index) {
+    if (!index) {
+      return s.toLowerCase()
+    }
+    return '-' + s.toLowerCase()
+  })
+}
+
+components.forEach(component => {
+  let arr = component.split('/')
+  let componentName = arr[arr.length - 1].replace(/(\.vue)/g, '')
+
+  // 以组件所在目录名称(短横线)作为 group
+  let group = getKebabCase(arr[arr.length - 2])
+  let path = getKebabCase(componentName)
+
+  routes.push({
+    path,
+    component: loadView(component),
+    meta: {
+      group
+    }
+  })
+})
+
+export default routes
